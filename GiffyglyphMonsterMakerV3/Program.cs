d2d5450750878444525e6e4b45c69abe28b0bcf2
Skyl3lazer <derekjjjj@gmail.com>
2022-11-28 17:54:16 -0500

    Some scaffolded stuff to maybe allow federated logins later

--- GiffyglyphMonsterMakerV3/Program.cs
+++ GiffyglyphMonsterMakerV3/Program.cs
@@ -8,21 +8,36 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var config = builder.Configuration;
 // Add services to the container.
-var connectionString = builder.Configuration["ConnectionStrings:ggmonstermaker"];
+var connectionString = config["ConnectionStrings:ggmonstermaker"];
 builder.Services.AddDbContextFactory<ApplicationDbContext>(item => item.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-
+/* maybe later
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        IConfigurationSection googleAuthNSection =
+            config.GetSection("Authentication:Google");
+        options.ClientId = googleAuthNSection["ClientId"];
+        options.ClientSecret = googleAuthNSection["ClientSecret"];
+    })
+    .AddTwitter(twitterOptions =>
+    {
+        twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
+        twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
+        twitterOptions.RetrieveUserDetails = true;
+    });
+*/
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddScoped<IPrintingService, PrintingService>();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
 builder.Services.AddScoped<MonsterService>();
 builder.Services.AddScoped<FeatureService>();
 
