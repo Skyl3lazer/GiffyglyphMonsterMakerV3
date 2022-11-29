57203cc0b9ff06b2e9488422cedbda3a36ef535b
Skyl3lazer <derekjjjj@gmail.com>
2022-11-29 13:36:43 -0500

    Small style changes

--- GiffyglyphMonsterMakerV3/Data/Countermeasure.cs
+++ GiffyglyphMonsterMakerV3/Data/Countermeasure.cs
@@ -15,7 +15,7 @@ namespace GiffyglyphMonsterMakerV3.Data
             string desc =
                 "<span class=\"text-white fa-solid p-1 " + (String.IsNullOrWhiteSpace(CustomIcon) ? Icon : CustomIcon) + " " + RarityStyle + "\"></span></span><span class=\"ms-1\">";
             desc += @"<span class=""fw-bold"">" + Name;
-            desc += "</span>: ";
+            desc += ":</span> ";
 
             //If you want to just totally override a thing, go for it
             if (!string.IsNullOrWhiteSpace(OverrideMarkup))
