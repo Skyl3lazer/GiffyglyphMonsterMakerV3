b694502681d18519ce1dedf9c98aee81ece9c142
Skyl3lazer <derekjjjj@gmail.com>
2022-11-30 10:49:01 -0500

    Added png downloader and button for column toggle

---
+++ GiffyglyphMonsterMakerV3/Utility/FileUtil.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace GiffyglyphMonsterMakerV3.Utility
+{
+    public static class FileUtil
+    {
+        public async static Task SaveAs(IJSRuntime js, string filename, byte[] data)
+        {
+            await js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+        }
+    }
+}
